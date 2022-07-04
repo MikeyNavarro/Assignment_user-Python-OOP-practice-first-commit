99975a5bc9de7232253a0469cedb93ac2ac582df
Mikey Navarro <mikey.navarro.135@gmail.com>
2022-07-03 23:26:26 -0700

    first commit

---
+++ AssignmentUser.py
@@ -0,0 +1,34 @@
+class User:
+  def __init__(self, first_name, last_name, email, age):
+    
+    self.first_name = first_name 
+    self.last_name = last_name  
+    self.email = email 
+    self.age = age 
+    self.is_rewards_member = False
+    self.gold_card_points =  0
+
+  def display_info(self):
+    print(self.first_name)
+    print(self.last_name)
+    print(self.email)
+    print(self.age)
+    print(self.is_rewards_member)
+    print(self.gold_card_points)
+
+  def enroll(self):
+    self.is_rewards_member = True
+    self.gold_card_points += 200
+
+  def spend_points(self,amount):
+    self.gold_card_points -= amount
+
+
+
+user1 = User("Mikey", "Navarro" , "sdfksl@gmail.com", 22)
+user1.enroll()
+user1.spend_points(50)
+user1.display_info()
+
+
+
